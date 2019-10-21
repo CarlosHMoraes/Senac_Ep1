019ad47c603a3e13436d1e54e9ff6d5ea3bab8c1
CarlosHMoraes <36535938+CarlosHMoraes@users.noreply.github.com>
2019-10-21 01:44:56 -0300

    arquivos ep1 senac arquitetura
    arquivos ep1 senac arquitetura

---
+++ ep1.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from datetime import datetime
+import json, requests, uuid
+
+client = MongoClient()
+
+client = MongoClient('localhost', 27017)
+
+db = client.ep1_database
+
+print("Teste 1")      
+
+response = requests.get("http://localhost:3000/api/v1/products.json?token=e0c91507ca69781a03c62aabd83246ed75e8ed0f498a4652")
+now = datetime.now()
+timestamp = now.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+
+print("Teste 2")
+
+itens = json.loads(response.text)
+
+for item in itens["products"]:
+    uid = uuid.uuid1()
+    produto = {
+        'unique_id': uid.hex,
+        'data': now,
+        'timestamp': timestamp,
+        'id': item['id'],
+        'name': item['name'],
+        'price': item['price']
+    }
+    db.ep1_collection.insert_one(produto)
+
+print("Teste 3")
